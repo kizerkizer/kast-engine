a7aef45cf13b1a80b325c5c0ee9ce2567a82301d
kizerkizer <kizerkizerkizerkizerkizer@gmail.com>
2018-06-19 13:45:21 +0000

    refactor cast.mjs

--- cast.mjs
+++ cast.mjs
@@ -1,90 +1,90 @@
 import * as globals from './globals.mjs';
 
+const maxCastDistance = 25;
+
 function getCastTheta (d, distanceFromCenter) {
   return Math.atan2(distanceFromCenter, d);
 }
 
 function cast (theta) {
   theta = globals.normalizeAngle(theta);
-  let v1 = globals.observer;
-  let h1 = globals.observer;
-  let current = globals.observer;
-  let hit = false;
-  let offset;
-  let which;
-  for (let i = 0; i < 10; i++) {
-    if (((theta >= 0 && theta <= Math.PI / 2) || (theta <= 0 && theta >= -Math.PI / 2))) {
-      v1 = rightDrawOneRay2(current, theta);
-    } else {
-      v1 = leftDrawOneRay2(current, theta);
-    }
-    
-    if (((theta >= 0 && theta <= Math.PI))) {
-      h1 = downDrawOneRay2(current, theta);
-    } else {
-      h1 = upDrawOneRay2(current, theta);
-    }
-    
-    if (globals.distance(current, v1) < globals.distance(current, h1)) {
-      current = v1;
-      if (globals.grid[globals.getCellUnderVertex(v1)[0]] && globals.grid[globals.getCellUnderVertex(v1)[0]][globals.getCellUnderVertex(v1)[1]] === 1) {
-        hit = true;
-        which = `vertical`;
-        offset = current[1] % globals.side;
-        break;
-      }
-    } else {
-      current = h1;
-      if (globals.grid[globals.getCellUnderVertex(h1)[0]] && globals.grid[globals.getCellUnderVertex(h1)[0]][globals.getCellUnderVertex(h1)[1]] === 1) {
-        hit = true;
-        which = `horizontal`;
-        offset = current[0] % globals.side;
-        break;
-      }
-    }
+
+  let intersection = globals.observer, 
+    hit = false,
+    offset,
+    which;
+
+  for (let i = 0; i < maxCastDistance && !hit; i++) {
+    ({which, offset, intersection} = getNextIntersection(intersection, theta));
+    hit = isCollision(intersection);
   }
+
   return {
-    vertex: current,
+    intersection,
     hit,
     offset,
     which
   };
 }
 
-function rightDrawOneRay2 (initialVertex, theta) {
-  theta = globals.normalizeAngle(theta);
-  let cellX = initialVertex[0] - globals.getCellUnderVertex(initialVertex)[0] * globals.side,
-    cellY = initialVertex[1] - globals.getCellUnderVertex(initialVertex)[1] * globals.side;
-  let h = globals.side - cellX + 1;
-  let v = Math.tan(theta) * h;
-  return [initialVertex[0] + h, initialVertex[1] + v];
+function isCollision (vertex) {
+  let cell = globals.getCellUnderVertex(vertex);
+  return globals.grid[cell[0]] && globals.grid[cell[0]][cell[1]] === 1;
 }
 
-function leftDrawOneRay2 (initialVertex, theta) {
-  theta = globals.normalizeAngle(theta);
-  let cellX = initialVertex[0] - globals.getCellUnderVertex(initialVertex)[0] * globals.side,
-    cellY = initialVertex[1] - globals.getCellUnderVertex(initialVertex)[1] * globals.side;
-  let h = cellX + 1;
-  let v =  Math.tan(theta) * h;
-  return [initialVertex[0] - h, initialVertex[1] - v];
+function getNextIntersection (initialVertex, theta) {
+  let intersectionHorizontal = getNextIntersectionHorizontal(initialVertex, theta),
+    intersectionVertical = getNextIntersectionVertical(initialVertex, theta);
+
+  if (globals.distance(initialVertex, intersectionHorizontal) < globals.distance(initialVertex, intersectionVertical)) {
+    return {
+      intersection: intersectionHorizontal,
+      which: `horizontal`,
+      offset: intersectionHorizontal[0] % globals.side
+    };
+  } else {
+    return {
+      intersection: intersectionVertical,
+      which: `vertical`,
+      offset: intersectionHorizontal[1] % globals.side
+    };
+  }
 }
 
-function upDrawOneRay2 (initialVertex, theta) {
+function getNextIntersectionHorizontal (initialVertex, theta) {
   theta = globals.normalizeAngle(theta);
-  let cellX = initialVertex[0] - globals.getCellUnderVertex(initialVertex)[0] * globals.side,
-    cellY = initialVertex[1] - globals.getCellUnderVertex(initialVertex)[1] * globals.side;
-  let v = cellY + 1;
-  let h = v / Math.tan(theta);
-  return [initialVertex[0] - h, initialVertex[1] - v];
+
+  let up = !((theta >= 0 && theta <= Math.PI)),
+    cell = globals.getCellUnderVertex(initialVertex);
+
+  let cellX = initialVertex[0] - cell[0] * globals.side,
+    cellY = initialVertex[1] - cell[1] * globals.side;
+
+  let y = up ? (cellY + 1) : (globals.side - cellY + 1),
+    x = y / Math.tan(theta);
+
+  y = up ? -y : y;
+  x = up ? -x : x;
+
+  return [initialVertex[0] + x, initialVertex[1] + y];
 }
 
-function downDrawOneRay2 (initialVertex, theta) {
+function getNextIntersectionVertical (initialVertex, theta) {
   theta = globals.normalizeAngle(theta);
-  let cellX = initialVertex[0] - globals.getCellUnderVertex(initialVertex)[0] * globals.side,
-    cellY = initialVertex[1] - globals.getCellUnderVertex(initialVertex)[1] * globals.side;
-  let v = globals.side - cellY + 1;
-  let h =  v / Math.tan(theta);
-  return [initialVertex[0] + h, initialVertex[1] + v];
+
+  let left = !((theta >= 0 && theta <= Math.PI / 2) || (theta <= 0 && theta >= -Math.PI / 2)),
+    cell = globals.getCellUnderVertex(initialVertex);
+
+  let cellX = initialVertex[0] - cell[0] * globals.side,
+    cellY = initialVertex[1] - cell[1] * globals.side;
+
+  let x = left ? (cellX + 1) : (globals.side - cellX + 1),
+    y = x * Math.tan(theta);
+
+  x = left ? -x : x;
+  y = left ? -y : y;
+
+  return [initialVertex[0] + x, initialVertex[1] + y];
 }
 
 export {
